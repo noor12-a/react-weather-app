693453190273872dc6188006c7f4792d44d96095
Noor <mahnoormalik778856@gmail.com>
2026-04-19 20:04:23 +0500

    react-weather-app

---
+++ src/components/search/index.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Search({search, setSearch, handleSearch}){
+    return  <div className = "search-engine">
+        <input 
+        type="text" 
+        placeholder="enter city name"
+        name = "search"
+        value={search}
+        onChange={(event)=> setSearch(event.target.value)}
+        />
+        <button onClick={handleSearch}>Search Weather</button>
+    </div>
+} 
